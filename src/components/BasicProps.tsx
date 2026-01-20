6287cb31cf9a9d9ba70ec49ecea0857e1f808fee
Rahul Pal <usefulltech1819@gmail.com>
2026-01-20 22:21:10 +0530

    Add other element

--- src/components/BasicProps.tsx
+++ src/components/BasicProps.tsx
@@ -1,10 +1,19 @@
+import { useState } from "react"
 import Button from "./Button"
 
 const BasicProps = () => {
+  const [count, setCount] = useState(0);
   return (
-    <div className="text-white">
-      <Button text="Click me!" color="primary" size="large" disabled={false} />
-    </div>
+    <section className="p-8 bg-white rounded-xl shadow-2xl">
+      <h2>Basic Props Example</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem officia eaque ad pariatur odio aut voluptas doloremque incidunt labore sequi!</p>
+      <div className="space-y-4">
+        <h3>Different colors</h3>
+        <div className="flex flex-wrap gap-3">
+          <Button text="Click me!" size="large" disabled={false} color="primary"/>
+        </div>
+      </div>
+    </section>
   )
 }
 
