7e522c3bc413abb23a34746a21d754eaa4991e08
Rahul Pal <usefulltech1819@gmail.com>
2026-01-21 22:51:18 +0530

    Fix CSS issus

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,15 +2,15 @@ import type { CardProp } from "../types/Card";
 
 const Card: React.FC<CardProp> = ({ children, title, color="blue" }) => {
   const colorClasses = {
-      blue: "border-blue-500 bg-blue-500",
-      green: "border-green-500 bg-green-500",
-      purple: "border-purple-500 bg-purple-500",
-      red: "border-red-500 bg-red-500"
+      blue: "border-blue-500 bg-blue-100",
+      green: "border-green-500 bg-green-100",
+      purple: "border-purple-500 bg-purple-100",
+      red: "border-red-500 bg-red-100"
   };
 
   return (
     <div className={`border-l-4 ${colorClasses[color]} p-6 rounded-lg shadow-md`}>
-      {title && <h3 className="text-xl font-bold mb-3 text-gray-600">{title}</h3>}
+      {title && <h3 className="text-xl font-bold mb-3 text-gray-800">{title}</h3>}
       <div className="text-gray-700">{children}</div>
     </div>
   )
