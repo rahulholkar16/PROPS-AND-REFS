b8eea68c9612d274e9643ebb9429a0df6723aa28
Rahul Pal <usefulltech1819@gmail.com>
2026-01-21 20:37:10 +0530

    Add cointaner component

--- src/components/ChildrenProps.tsx
+++ src/components/ChildrenProps.tsx
@@ -1,6 +1,20 @@
+import type { ContainerProp } from "../types/Cointainer";
+
+const Cointainer: React.FC<ContainerProp> = ({ children, layout='vertical' }) => {
+  const layoutClasses = {
+    vertical: "flex flex-col space-y-4",
+    horizontal: "flex flex-row flex-wrap gap-4",
+    grid: "grid grid-cols-1 md:grid-cols-2 gap-4"
+  };
+  
+  return (
+    <div className={layoutClasses[layout]}>{children}</div>
+  )
+}
+
 const ChildrenProps = () => {
   return (
-    <div>ChildrenProps</div>
+    <section className="p-8 bg-white rounded-xl shadow-lg"></section>
   )
 }
 
