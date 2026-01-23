72807c233355f8a2b6de479c167533a3baf6ad91
Rahul Pal <usefulltech1819@gmail.com>
2026-01-23 21:48:38 +0530

    Add theme toggler component

--- src/components/ThemeToggler.tsx
+++ src/components/ThemeToggler.tsx
@@ -1,7 +1,26 @@
+import useTheme from "../hook/useTheme";
+
 const ThemeToggler = () => {
+  const { theme, toggleTheme, isDark } = useTheme();
   return (
-    <div>ThemeToggler</div>
-  )
+      <button
+          onClick={toggleTheme}
+          className={`
+        relative w-16 h-8 rounded-full transition-colors duration-300 mt-3
+        ${isDark ? "bg-blue-600" : "bg-gray-300"}
+      `}
+      >
+          <div
+              className={`
+          absolute top-1 left-1 w-6 h-6 rounded-full bg-white
+          transition-transform duration-300 flex items-center justify-center
+          ${isDark ? "transform translate-x-8" : ""}
+        `}
+          >
+              {isDark ? "🌙" : "☀️"}
+          </div>
+      </button>
+  );
 }
 
 export default ThemeToggler;
